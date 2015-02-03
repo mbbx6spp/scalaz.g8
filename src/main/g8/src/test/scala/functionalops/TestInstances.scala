443321204b36fdf0bd506d0db02c2a1acbec3879
Susan Potter <me@susanpotter.net>
2015-02-03 13:29:08 -0800

    Fix typo in TestInstances source template

--- src/main/g8/src/test/scala/functionalops/TestInstances.scala
+++ src/main/g8/src/test/scala/functionalops/TestInstances.scala
@@ -2,7 +2,7 @@ package $orgId$.$name$
 
 import org.scalacheck.{Gen, Arbitrary}
 
-import $orgId$.$name$.$subpackage$._
+import $orgId$.$name$.$subproject$._
 
 object TestInstances {
   // TODO Define typeclass instances needed for Scalacheck
